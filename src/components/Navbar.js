674d7be6f4fbd46c0b5b526650814715fd4a3cb6
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-06-30 00:01:14 +0530

    navbar fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import { graphql, useStaticQuery } from 'gatsby';
 
@@ -13,15 +13,21 @@ const Navbar = () => {
             }
         }
     `);
+    const [open, setOpen] = useState(false);
+    const navBarCollapse = () => {
+        return {
+            display: open ? 'block' : 'none',
+        }
+    }
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
                 <Link className="navbar-brand" to='/'>{data.site.siteMetadata.title}</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <button onClick={() => { setOpen(!open) }} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarColor01">
+                <div style={navBarCollapse()} className="collapse navbar-collapse" id="navbarColor01">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="/">Home</Link>
